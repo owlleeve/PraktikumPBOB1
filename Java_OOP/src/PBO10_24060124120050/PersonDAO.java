d4202456b3a0b9776cdf96e693280784f43edca7
owlleeve <oliviaoktaviani10@gmail.com>
2026-06-05 00:45:22 +0700

    Membersihkan branch PBO12

--- Java_OOP/src/PBO10_24060124120050/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-/**
- * File: PersonDAO.java
- * Deskripsi: interface untuk person access object
- */
-package PBO10_24060124120050;
-
-public interface PersonDAO {
-    public void savePerson (Person p) throws Exception;
-}
